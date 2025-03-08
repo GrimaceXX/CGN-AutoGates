ad6c31f22de17e4570f22d867e0b990c92d15a46
Grimace <75762743+GrimaceXX@users.noreply.github.com>
2025-03-08 16:44:17 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+--[[
+   ____ ____ _   _   ____            _       _       
+  / ___/ ___| \ | | / ___|  ___ _ __(_)_ __ | |_ ___ 
+ | |  | |  _|  \| | \___ \ / __| '__| | '_ \| __/ __|
+ | |__| |_| | |\  |  ___) | (__| |  | | |_) | |_\__ \
+  \____\____|_| \_| |____/ \___|_|  |_| .__/ \__|___/
+                                      |_|            
+]]
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'GrimaceXX'
+description 'FiveM resource by GrimaceXX'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
